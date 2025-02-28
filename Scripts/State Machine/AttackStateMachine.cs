a54b5e714a68a556054b50a6721e1605def8d2d5
Vadim Niedental <vadim.niedental@gmail.com>
2025-02-28 12:33:39 +0100

    Attack State Machine to handle different attack states and rotate the sprite  - Uses simple states instead of blend tree.

---
+++ Scripts/State Machine/AttackStateMachine.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class AttackStateMachine : MonoBehaviour
+{
+    [SerializeField] Attack Attack;
+    [SerializeField] private Animator attackAnimator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Camera mainCamera;
+
+    private Vector2 lookDirection;
+    private readonly string isNormalAttacking1Param = "isNormalAttacking1";
+    private readonly string isNormalAttacking2Param = "isNormalAttacking2";
+    private readonly string isNormalAttacking3Param = "isNormalAttacking3";
+
+    private void HandleAnimator()
+    {
+        attackAnimator.SetBool(isNormalAttacking1Param, Attack.isNormalAttacking1);
+        attackAnimator.SetBool(isNormalAttacking2Param, Attack.isNormalAttacking2);
+        attackAnimator.SetBool(isNormalAttacking3Param, Attack.isNormalAttacking3);
+    }
+    
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        HandleMouseDirection();
+        HandleAttackRotation();
+        HandleAnimator();
+    }
+
+    private void HandleMouseDirection()
+    {
+        Vector3 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        lookDirection = (mousePos - transform.position).normalized;
+    }
+
+    private void HandleAttackRotation()
+    {
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        
+        transform.rotation = Quaternion.Euler(0f, 0f, angle -70f);
+    }
+
+}
