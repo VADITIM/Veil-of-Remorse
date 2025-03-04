59471cfa8d0c0d63332bfbe3e5f391cbf51d2faf
Vadim Niedental <vadim.niedental@gmail.com>
2025-03-04 22:43:45 +0100

    Shortened code.

--- Scripts/Player/Movement/Movement.cs
+++ Scripts/Player/Movement/Movement.cs
@@ -19,22 +19,11 @@ public class Movement : MonoBehaviour
     {
         movement = Vector2.zero;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            movement.y = 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            movement.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            movement.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            movement.x = 1;
-        }
+        if (Input.GetKey(KeyCode.W)) { movement.y = 1; }
+        if (Input.GetKey(KeyCode.S)) { movement.y = -1; }
+        if (Input.GetKey(KeyCode.A)) { movement.x = -1; }
+        if (Input.GetKey(KeyCode.D)) { movement.x = 1; }
+
 
         movement = movement.normalized;
 
