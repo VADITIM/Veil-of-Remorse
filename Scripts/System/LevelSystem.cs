360868e72e7c965255742198c253c71919f3f892
Vadim Niedental <vadim.niedental@gmail.com>
2025-03-05 14:43:46 +0100

    added skillpoints

--- Scripts/System/LevelSystem.cs
+++ Scripts/System/LevelSystem.cs
@@ -3,65 +3,85 @@ using UnityEngine;
 public class LevelSystem : MonoBehaviour
 {
     private int level = 1;
+    public int skillPoints = 0;
     private int experience = 0;
-    private int experienceToNextLevel = 100;
+    private int experienceToNextLevel;
     
     public int Level => level;
+    public int ExperienceToNextLevel => experienceToNextLevel;
     
-    public int GetExperience()
+    public int GetSkillPoints() { return skillPoints; }
+    public int GetExperience() { return experience; }
+    public int GetExperienceToNextLevel() { return experienceToNextLevel; }
+
+    void Start()
     {
-        return experience;
+        SaveData();
+        experienceToNextLevel = level * 100;
+    }
+
+    void Update()
+    {
+        ShowLevel();
     }
 
     public void GainExperience(int amount)
     {
         experience += amount;
-        Debug.Log("Gained " + amount + " experience. Current experience: " + experience);
-        if (experience >= experienceToNextLevel)
+        Debug.Log($"Gained: {amount} XP.\nNeeded: {experienceToNextLevel - experience}.");
+        while (experience >= experienceToNextLevel)
         {
             LevelUp();
         }
     }
     
-    void Start()
+    public void LevelUp()
+    {
+        level++;
+        skillPoints++;
+        experience -= experienceToNextLevel;
+        experienceToNextLevel = level * 100;
+        Debug.Log("\nLEVELED UP: " + Level);
+    }
+
+    public void UseSkillPoints(int amount)
+    {
+        skillPoints -= amount;
+    }
+
+    private void SaveData()
     {
         PlayerData data = SaveSystem.LoadGame();
         if (data != null)
         {
             level = data.level;
             experience = data.experience;
+            experienceToNextLevel = data.experienceToNextLevel; 
             transform.position = new Vector3(data.checkpointX, data.checkpointY, 0);
         }
     }
 
-    private void LevelUp()
+    public void LoadSavedData(PlayerData data)
     {
-        level++;
-        experience -= experienceToNextLevel;
-        experienceToNextLevel += 10;
-        Debug.Log("Level up! Current level: " + Level);
-    }
-
-    void Update()
-    {
-        ShowLevel();
-        ShowNextXP();
+        level = data.level;
+        experience = data.experience;
+        experienceToNextLevel = data.experienceToNextLevel;
+        
+        Debug.Log($"\nLevel and XP reset to last save: Level {level}, XP {experience}.");
     }
 
     private void ShowLevel()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            Debug.LogWarning("Current level: " + level);
-            Debug.LogWarning("Current experience: " + experience);
+            Debug.LogWarning("\nLevel: " + level);
+            Debug.LogWarning("\nXP: " + experience);
         }
-    }
-    private void ShowNextXP()
-    {
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            Debug.LogWarning("Max experience: " + experienceToNextLevel);
-            Debug.LogWarning("Experience to next level: " + (experienceToNextLevel - experience));
+            Debug.LogWarning("\nMax XP: " + experienceToNextLevel);
+            Debug.LogWarning("\nXP needed: " + (experienceToNextLevel - experience));
         }
     }
 }
